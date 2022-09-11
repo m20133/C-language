422c4787bf6a06025410eec420dbe9f1df3af8d6
Mitsuhashi Maika <m20133@g.metro-cit.ac.jp>
2022-09-11 22:33:51 +0900

    ｂ

---
+++ 新しいフォルダー/125.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int maina(void)
+{
+    int max, min, sum;
+    printf("最小値を入力してください");
+    scanf("%d", &min);
+    printf("最大値を入力してください");
+    scanf("%d", &max);
+
+    sum = (max + min) * (max - min + 1) / 2;
+
+    printf("%d~%dの合計は%dです",min, max, sum);
+
+
+}
---
+++ 新しいフォルダー/92.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 10000, b = 500, c = 3;
+
+    printf("%5d\n", a);
+    printf("%5d\n", b);
+    printf("%5d\n", c);
+
+    return 0;
+}
